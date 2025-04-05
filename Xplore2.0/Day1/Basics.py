978f69f648fe8cec43fcaf699d14d85f2d88fbdf
meekhumor <om17092004@gmail.com>
2025-04-05 22:30:07 +0530

    Update image processing scripts

--- Xplore2.0/Day1/Basics.py
+++ Xplore2.0/Day1/Basics.py
@@ -7,51 +7,51 @@ cv2.imshow('Original Image', image)
 
 # -------------------- IMAGE BASICS --------------------
 
-# shape of the image
-print("Image shape:", image.shape)
+# # shape of the image
+# print("Image shape:", image.shape)
 
-height, width, channels = image.shape
-print("Height:", height)
-print("Width:", width)
-print("Channels (BGR):", channels)
+# height, width, channels = image.shape
+# print("Height:", height)
+# print("Width:", width)
+# print("Channels (BGR):", channels)
 
-# accessing pixel value
-pix = image[100, 100]
-print("Pixel value at (100,100):", pix)
+# # accessing pixel value
+# pix = image[100, 100]
+# print("Pixel value at (100,100):", pix)
 
 
 # ------------------- SPLITTING CHANNELS --------------------
 
-# split image into blue, green, red channels
-blue, green, red = cv2.split(image)
+# # split image into blue, green, red channels
+# blue, green, red = cv2.split(image)
 
-zeros = np.zeros_like(blue)
+# zeros = np.zeros_like(blue)
 
-only_blue = cv2.merge([blue, zeros, zeros])
-cv2.imshow("Blue", only_blue)
+# only_blue = cv2.merge([blue, zeros, zeros])
+# cv2.imshow("Blue", only_blue)
 
-only_green = cv2.merge([zeros, green, zeros])
-cv2.imshow("Green", only_green)
+# only_green = cv2.merge([zeros, green, zeros])
+# cv2.imshow("Green", only_green)
 
-only_red = cv2.merge([zeros, zeros, red])
-cv2.imshow("Red", only_red)
+# only_red = cv2.merge([zeros, zeros, red])
+# cv2.imshow("Red", only_red)
 
 # ------------------- BASIC OPERATIONS -------------------
 
-# resize
-resized = cv2.resize(image, (300, 300))
-cv2.imshow('Resized Image', resized)
+# # resize
+# resized = cv2.resize(image, (300, 300))
+# cv2.imshow('Resized Image', resized)
 
-# crop
-cropped = image[50:200, 100:300]
-cv2.imshow('Cropped Image', cropped)
+# # crop
+# cropped = image[50:200, 100:300]
+# cv2.imshow('Cropped Image', cropped)
 
-# flip (0: vertical, 1: horizontal, -1: both)
-flipped = cv2.flip(image, 1)
-cv2.imshow('Flipped Image', flipped)
+# # flip (0: vertical, 1: horizontal, -1: both)
+# flipped = cv2.flip(image, 1)
+# cv2.imshow('Flipped Image', flipped)
 
-# saving image
-cv2.imwrite('flipped_image.jpg', flipped)
+# # saving image
+# cv2.imwrite('flipped_image.jpg', flipped)
 
 
 # Cleanup
